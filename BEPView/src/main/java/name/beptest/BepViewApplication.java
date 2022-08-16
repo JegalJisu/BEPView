c1a8635165b33d378ddf0b826fb3ee8267a5965c
JegalJisu <rkfwltn12@naver.com>
2022-08-16 18:47:59 +0900

    Spring boot 프로젝트 세팅

---
+++ BEPView/src/main/java/name/beptest/BepViewApplication.java
@@ -0,0 +1,13 @@
+package name.beptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BepViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BepViewApplication.class, args);
+	}
+
+}
---
+++ BEPView/src/test/java/name/beptest/BepViewApplicationTests.java
@@ -0,0 +1,13 @@
+package name.beptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BepViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
